c46581590469061b962a1617512e77c5861b31b3
maeva-12 <maevafouen@gmail.com>
2024-01-04 14:52:01 +0100

    Ajout du workflow de build et test

--- Documents/TITANIC/bcancer.py
+++ Documents/TITANIC/bcancer.py
@@ -16,7 +16,7 @@ y = pd.Series(data.target)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Créer et entraîner le modèle de régression logistique
-model = LogisticRegression()
+model = LogisticRegression(max_iter=10000)
 model.fit(X_train, y_train)
 
 # Évaluer le modèle
@@ -24,8 +24,8 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Précision du modèle : {accuracy:.2f}")
 
-# Sauvegarder le modèle entraîné au format pickle
-with open('breast_cancer_model.pkl', 'wb') as model_file:
-    pickle.dump(model, model_file)
+# Sauvegarder le modèle entraîné
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
 
-print("Modèle sauvegardé avec succès.")
+print("Modèle entraîné et sauvegardé.")
--- Documents/TITANIC/test.py
+++ Documents/TITANIC/test.py
@@ -3,35 +3,26 @@ import pickle
 from sklearn.metrics import accuracy_score
 
 # Charger le modèle
-with open('breast_cancer_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 # Charger ou préparer les données de test
-url = 'https://raw.githubusercontent.com/AbdallahTayeb/DevOps-Course/main/sample.csv'
-test_data = pd.read_csv(url)
-
-# Supprimer la colonne cible (si elle existe)
-if 'target' in test_data.columns:
-    test_data.drop('target', axis=1, inplace=True)
+df = pd.read_csv('sample.csv',sep=';')
+X_test = df.drop('target', axis=1)
+y_test = df['target']
 
 # Faire des prédictions
-y_pred = model.predict(test_data)
+y_pred = model.predict(X_test)
 
 # Calculer la précision
-# Note : Vous aurez besoin de véritables valeurs cibles pour calculer la précision
-# Si le fichier sample.csv contient également la colonne target, vous pouvez la comparer avec les prédictions
-# Sinon, vous devrez ajuster cette partie en fonction de vos besoins.
-
-# Charger les vraies valeurs cibles depuis le fichier sample.csv (si disponible)
-true_values = test_data['target']  # Assurez-vous que la colonne target existe dans le fichier sample.csv
-
-# Calculer la précision
-accuracy = accuracy_score(true_values, y_pred)
-print(f"Précision du modèle sur les nouvelles données : {accuracy:.2f}")
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Précision du modèle : {accuracy:.2f}")
 
 # Définir un seuil de classification
-seuil = 0.8
+seuil_classification = 0.90  # par exemple
 
 # Vérifier si le seuil est atteint
-predictions_at_seuil = (model.predict_proba(test_data)[:, 1] > seuil).astype(int)
-print(f"Précision avec seuil {seuil} : {accuracy_score(true_values, predictions_at_seuil):.2f}")
+if accuracy >= seuil_classification:
+    print("Le seuil de classification est atteint. Le modèle est prêt pour le déploiement.")
+else:
+    print("Le seuil de classification n'est pas atteint. Le modèle nécessite une amélioration.")
